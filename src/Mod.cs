46adfe1434d46b8a61aa5814aba237adaf370c9e
thalber <73020111+thalber@users.noreply.github.com>
2023-03-10 20:29:59 +0400

    slightly improve descriptor scanner

--- src/Mod.cs
+++ src/Mod.cs
@@ -1,47 +1,51 @@
+#pragma warning disable CS0618
+[assembly: System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.RequestMinimum)]
+#pragma warning restore CS0618
+
 namespace Poly;
 
 using static MoonSharp.Interpreter.ModuleRegister;
 
 [BIE.BepInPlugin("rwmodding.coreorg.poly", "Poly", "0.1")]
 public class Mod : BIE.BaseUnityPlugin {
+
 	//todo: register all types in assemblycsharp for interop
 	internal static BIE.Logging.ManualLogSource __logger = default!;
-	internal static RFL.Assembly __asmcshaprAsm = typeof(AboveCloudsView).Assembly;
-	internal static RFL.Assembly __unitycoreAsm = typeof(Vector2).Assembly;
-	internal static Dictionary<Type, LUA.Interop.IUserDataDescriptor> __asmCsharpDescriptors = new();
-	internal static Dictionary<Type, LUA.Interop.IUserDataDescriptor> __unityEngineCoreDescriptors = new();
-	internal static THR.Tasks.Task? __collectDescriptorsTask = null;
-	//internal static THR.SemaphoreSlim __descriptorCollectionLock = new(0, 1);
+	internal RFL.Assembly _asmcshaprAsm = typeof(AboveCloudsView).Assembly;
+	internal RFL.Assembly _unitycoreAsm = typeof(Vector2).Assembly;
+	internal Dictionary<Type, LUA.Interop.IUserDataDescriptor> _typeDescriptors = new();
+	internal Dictionary<Type, LUA.DynValue> _staticDescriptors = new();
+	internal THR.Tasks.Task? _collectDescriptorsTask = null;
 
 	public void OnEnable() {
 		On.RainWorld.OnModsInit += Init; Logger.LogMessage("Poly is running MoonSharp!\n" + LUA.Script.GetBanner());
 		LUA.Script.WarmUp();
 		LUA.Script.DefaultOptions.ScriptLoader = new AssetScriptLoader();
 		LUA.UserData.DefaultAccessMode = LUA.InteropAccessMode.Preoptimized;
-		__collectDescriptorsTask = RegisterDescriptors();
+		_collectDescriptorsTask = THR.Tasks.Task.Run(() => RegisterDescriptors());
 	}
-	public async THR.Tasks.Task RegisterDescriptors() {
-		async THR.Tasks.Task registerType(Type t) {
+	public void RegisterDescriptors() {
+		void registerType(Type t) {
 			List<THR.Tasks.Task> nestedTasks = new();
-			Dictionary<Type, LUA.Interop.IUserDataDescriptor>? selectedDict = t.Assembly switch {
-				_ when t.Assembly == __asmcshaprAsm => __asmCsharpDescriptors,
-				_ when t.Assembly == __unitycoreAsm => __unityEngineCoreDescriptors,
-				_ => null
-			};
-			if (selectedDict is null) return;
-			lock (selectedDict) {
-				if (selectedDict.ContainsKey(t)) return;
-				selectedDict[t] = LUA.UserData.RegisterType(t);
+			lock (_typeDescriptors) {
+				_typeDescriptors[t] = LUA.UserData.RegisterType(t);
+			}
+			lock (_staticDescriptors) {
+				_staticDescriptors[t] = LUA.UserData.CreateStatic(t);
 			}
 			foreach (Type nested in t.GetNestedTypes(BF_ALL_CONTEXTS)) {
-				nestedTasks.Add(registerType(nested));
+				nestedTasks.Add(THR.Tasks.Task.Run(() => registerType(nested)));
 			}
 			THR.Tasks.Task.WaitAll(nestedTasks.ToArray());
 		}
-		List<THR.Tasks.Task> typeTasks = new();
-		foreach (Type t in typeof(AboveCloudsView).Assembly.GetTypesSafe(out RFL.ReflectionTypeLoadException? err)) {
-			typeTasks.Add(registerType(t));
+		void scanTypes(List<THR.Tasks.Task> typeTasks, IEnumerable<Type> types) {
+			foreach (Type t in types) {
+				typeTasks.Add(THR.Tasks.Task.Run(() => registerType(t)));
+			}
 		}
+		List<THR.Tasks.Task> typeTasks = new();
+		scanTypes(typeTasks, typeof(AboveCloudsView).Assembly.GetTypesSafe(out RFL.ReflectionTypeLoadException? err));
+		scanTypes(typeTasks, typeof(Vector2).Assembly.GetTypesSafe(out RFL.ReflectionTypeLoadException? err1));
 		THR.Tasks.Task.WaitAll(typeTasks.ToArray());
 	}
 
@@ -50,15 +54,18 @@ public class Mod : BIE.BaseUnityPlugin {
 			orig(self);
 			__logger = Logger;
 			try {
-				THR.Tasks.Task.WaitAll(__collectDescriptorsTask);
+				THR.Tasks.Task.WaitAll(_collectDescriptorsTask);
 			}
-			catch (AggregateException ex){
+			catch (AggregateException ex) {
 				__logger.LogError(ex);
-			} 
+			}
+			finally {
+				_collectDescriptorsTask = null;
+			}
 			const string startup_folder = "lua-startup";
 			foreach (string file in AssetManager.ListDirectory(startup_folder, false, false)) {
 				try {
-					LUA.DynValue crw_lua = LUA.UserData.Create(self, __asmCsharpDescriptors[typeof(RainWorld)]);
+					LUA.DynValue crw_lua = LUA.UserData.Create(self, _typeDescriptors[typeof(RainWorld)]);
 					IO.FileInfo fi = new(file);
 					if (fi.Extension is not ".lua") continue;
 					__logger.LogMessage($"Running startup script {fi.Name}");
@@ -80,29 +87,5 @@ public class Mod : BIE.BaseUnityPlugin {
 		script.Globals["bepin"] = script.Globals.RegisterModuleType(typeof(LuaModules.Bepin));
 		script.Globals["assets"] = script.Globals.RegisterModuleType(typeof(LuaModules.Assets));
 		return script;
-	}
-	public static LUA.Script? InitScriptSource(string source) {
-		LUA.Script res = InitBlankScript();
-		try {
-			res.LoadString(source);
-			return res;
-		}
-		catch (Exception ex) {
-			__logger.LogError($"Could not load script from source due to error: {ex}");
-			return null;
-		}
-	}
-	public static LUA.Script? InitScriptAssetpath(string asset) {
-		LUA.Script res = InitBlankScript();
-		try {
-			res.LoadFile(asset);
-
-			//res.Call()
-			return res;
-		}
-		catch (Exception ex) {
-			__logger.LogError($"Could not load script from file due to error: {ex}");
-			return null;
-		}
 	}
 }
